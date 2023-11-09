cb97367bae968a070350d9fa73ece9a3d7352a66
Burenzaya <bvrnee1009@gmail.com>
2023-11-09 12:00:57 +0800

    chaining data

--- hash.c
+++ hash.c
@@ -5,34 +5,48 @@
 #define TABLE_SIZE 10
 
 typedef struct Student {
-    char key[8]; // Assuming key format like "20B1NUM"
-    int student_id; // Assuming student ID is an integer
-    struct Student* next; // For handling collisions
+    int key;
+    char student_id[12];
+    struct Student* next;
 } Student;
 
 Student* hash_table[TABLE_SIZE] = {NULL};
 
-int hash(char* key) {
-    return atoi(&key[4]) % TABLE_SIZE; // Use last 4 digits for hashing
+void insert_student(char* data);
+void print_table();
+
+int main() {
+    insert_student("20B1NUM0362");
+    insert_student("20B1NUM1745");
+    insert_student("20B1NUM2362");
+    insert_student("20B1NUM5362");
+    insert_student("20B1NUM2145");
+    insert_student("20B1NUM0862");
+    insert_student("20B1NUM3362");
+    insert_student("20B1NUM9945");
+    insert_student("20B1NUM4562");
+    insert_student("20B1NUM3768");
+    insert_student("20B1NUM2912");
+
+    print_table();
+
+    return 0;
 }
 
 void insert_student(char* data) {
-    char key[8];
-    strncpy(key, data, 7); // Copy the first 7 characters (e.g., "20B1NUM")
-
-    int student_id = atoi(&data[7]); // Get the last 4 digits as student ID
-
-    int index = hash(key);
+    char first_digit = data[7];
+    int index = first_digit - '0';
 
     Student* new_student = (Student*)malloc(sizeof(Student));
-    strcpy(new_student->key, key);
-    new_student->student_id = student_id;
+
+    new_student->key = index;
+    strcpy(new_student->student_id, data);
+
     new_student->next = NULL;
 
     if (hash_table[index] == NULL) {
         hash_table[index] = new_student;
     } else {
-        // Handle collision by chaining
         Student* current = hash_table[index];
         while (current->next != NULL) {
             current = current->next;
@@ -43,26 +57,17 @@ void insert_student(char* data) {
 
 void print_table() {
     for (int i = 0; i < TABLE_SIZE; i++) {
-        printf("Slot %d: ", i);
-        Student* current = hash_table[i];
-        while (current != NULL) {
-            printf("%s%d -> ", current->key, current->student_id);
-            current = current->next;
+        if (hash_table[i] != NULL) {
+            Student* current = hash_table[i];
+
+            printf("Slot %d:\t", i);
+
+            while (current != NULL) {
+                printf("%s\t", current->student_id);
+                current = current->next;
+            }
+
+            printf("\n");
         }
-        printf("NULL\n");
     }
-}
-
-int main() {
-    char data1[] = "20B1NUM0362";
-    char data2[] = "20B1NUM1745";
-    char data3[] = "20C4NUM234";
-
-    insert_student(data1);
-    insert_student(data2);
-    insert_student(data3);
-
-    print_table();
-
-    return 0;
 }
